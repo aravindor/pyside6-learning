df159e1de1d1cf1d4fb02ffd2e0012b947411488
Luth3r <aravindor@gmail.com>
2023-09-07 15:25:50 +0530

    Icons from resource file added

--- plus_minus_form.py
+++ plus_minus_form.py
@@ -1,6 +1,7 @@
 from PySide6 import QtWidgets, QtGui
 
 from ui.ui_plus_minus_form import Ui_PlusMinusForm
+import ui.plus_minus_resource_rc
 
 
 class PlusMinusForm(QtWidgets.QWidget, Ui_PlusMinusForm):
@@ -9,6 +10,10 @@ class PlusMinusForm(QtWidgets.QWidget, Ui_PlusMinusForm):
         self.setupUi(self)
         self.setWindowTitle("Plus minus form")
         self.spin_box.setValue(50)
+        plus_icon = QtGui.QIcon(":/images/plus.png")
+        minus_icon = QtGui.QIcon(":/images/minus.png")
+        self.minus_button.setIcon(minus_icon)
+        self.plus_button.setIcon(plus_icon)
         self.minus_button.clicked.connect(self.plus)
         self.plus_button.clicked.connect(self.minus)
 
